b31692211c0f6234a99c90fee27f408b82f9f9dd
ujcheon <ujcheon@sookmyung.ac.kr>
2019-11-22 20:23:47 +0900

    commit

--- 3th_seminar/seminar21/Pods/Target Support Files/Pods-seminar21-seminar21UITests/Pods-seminar21-seminar21UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seminar21_seminar21UITests : NSObject
-@end
-@implementation PodsDummy_Pods_seminar21_seminar21UITests
-@end
--- 3th_seminar/seminar21/Pods/Target Support Files/Pods-seminar21/Pods-seminar21-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seminar21 : NSObject
-@end
-@implementation PodsDummy_Pods_seminar21
-@end
