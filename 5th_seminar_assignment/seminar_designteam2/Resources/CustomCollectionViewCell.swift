f490832ec87f937daf364e5d7afe9b5ea81fa335
ujcheon <ujcheon@sookmyung.ac.kr>
2019-11-23 16:01:42 +0900

    변경

---
+++ 5th_seminar_assignment/seminar_designteam2/Resources/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  seminar_designteam2
+//
+//  Created by 천유정 on 23/11/2019.
+//  Copyright © 2019 yujeong. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
